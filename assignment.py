37da5436b0124ae2b159073c14e5fcea6b06f928
Mary Ayako Khamati <ayakomary42@gmail.com>
2026-03-12 13:41:40 +0300

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,35 @@
+# Week 4 AI Assignment
+# Name: Mary
+
+# Question 4
+# Write a program that prints the message 
+
+print("Hello AI Students")
+
+# Question 5
+# Create variables and print them
+
+Name= "Mary"
+Age= 22
+City= "Nairobi"
+Course= "AI"
+
+print("Name:", Name)
+print("Age:", Age)
+print("City:", City)
+
+# Question 6
+# Create a list of programming languages
+
+language=["Python", "JavaScript", "C++", "C", "Java", "SQL", "R", "Go" , "C#"]
+
+# Print the first language in the list
+print("First language:", language[0])
+
+# Question 7
+# Create a dictionary for student information
+
+student={"Name":"Mary"
+         }
+# Print the student's name
+print("Student name:", student["Name"])
